59eb3eb8abf35fdd041c72a95615e48b38053262
Jeff McMillen <jmcmillenmusic@gmail.com>
2024-03-21 23:50:39 -0500

    Set up and tested functions for placing bets

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 // Imports variables from their respective scripts
 import { deck, shuffle } from "./scripts/shuffle.js";
+import { playerMoney, playerBet, increaseBet, decreaseBet, maximumBet, refresh } from "./scripts/playerMoney.js";
 
 // Shuffles the deck automatically when the window loads 
 window.onload = shuffle();
 console.log(deck);
+
+// Show the player how much money they have and how much they're betting
+document.getElementById("playerMoney").textContent = `Cash: ${playerMoney}`;
+document.getElementById("playerBet").textContent = `Bet: ${playerBet}`;
+
+// Adds event listeners to buttons to increase/decrease bid or bid the maximum amount
+document.getElementById("increaseBet").addEventListener('click', increaseBet);
+document.getElementById("increaseBet").addEventListener('click', refresh);
+document.getElementById("decreaseBet").addEventListener('click', decreaseBet);
+document.getElementById("decreaseBet").addEventListener('click', refresh);
+document.getElementById("maximumBet").addEventListener('click', maximumBet);
+document.getElementById("maximumBet").addEventListener('click', refresh);
